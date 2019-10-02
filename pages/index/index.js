9968771ab367e8a8d82b25f8f706a0b2cb193ac7
QingboTian <tqb820965236@163.com>
2019-10-02 23:58:54 +0800

    微信小程序网易云模板的练习

---
+++ pages/index/index.js
@@ -0,0 +1,15 @@
+const app = getApp()
+
+Page({
+  // 轮播图
+  data: {
+    bgs : [
+      {
+        url : "../../image/bg1.jpg"
+      },
+      {
+        url: "../../image/bg2.jpg"
+      }
+    ]
+  }
+})
